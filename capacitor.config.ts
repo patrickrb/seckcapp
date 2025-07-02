fe6d9f969b153fc4040f348eb111615d9c4c58e7
Patrick Burns <burnsoft@gmail.com>
2025-07-02 14:45:15 -0500

    update settings for build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'org.seckc.mobileapp',
   appName: 'seckcapp',
-  webDir: 'build'
+  webDir: 'dist'
 };
 
 export default config;
